e7c9591c4c73b05d89d4b9b487f6e8c615d7a0fd
macribo <roibeard.mac.unfraidh@gmail.com>
2019-12-15 15:45:15 +0100

    update mob portrait bg.

--- public/js/dm-geaga.js
+++ public/js/dm-geaga.js
@@ -1,13 +1,37 @@
 $(document).ready(function () {
 // alert();
+let prevStoryStep = 'inbound from cookie || null'
+let localStoryStep = 0;
+let dmGeaga = [
+
+    `<button>Cé tú féin? </button><button> Cé tusa?</button>`,``,``,``
+];
 $('#shout').on('touchend', function(){
 
-    if($('#shout').html('visible')){
-        alert('kungfu')
+   begin();
+        // alert('kungfu')
     $('#hero-keyboard').fadeOut();
-    $('#keyboard-container').fadeIn();
+    
+}
+);
+begin = () =>{
+    localStoryStep++;
+    // $('#output').html(dmGeaga[1]);
+    // alert('dmGeaga[1]');
+    render();
+}
+
+
+
+render=()=>{
+
+if(localStoryStep === 1){
+$('#keyboard-container').fadeIn();
+// alert(localStoryStep);
+    $('#output').html(`${dmGeaga[0]}`);
 
 }
-});
+}
 
+render();
 });
